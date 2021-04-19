625b8b0bbc73db9e5447e97873d434c1f1d45714
wangzun <wangzun0009@gmail.com>
2021-04-19 17:29:46 +0800

    cmd

---
+++ cmd/protoc-gen-gamerpc/go.mod
@@ -0,0 +1,5 @@
+module gamerpc
+
+go 1.13
+
+require google.golang.org/protobuf v1.26.0
---
+++ cmd/protoc-gen-gamerpc/make.sh
@@ -0,0 +1,2 @@
+go build -o protoc-gen-gamerpc .
+cp /Users/wangzun/code/gamerpc/protoc-gen-gamerpc /Users/wangzun/go/bin/protoc-gen-gamerpc
